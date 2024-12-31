887c6b5cf3f9293ed5c98f6f9db2842b45a628bb
CarysaPeeters <carysa.peeters@student.ehb.be>
2024-12-31 12:46:00 +0100

    docs: added comments to the retrieveData.js to explain what is happening in the file

--- scripts/js/retrieveData.js
+++ scripts/js/retrieveData.js
@@ -1,3 +1,7 @@
+// The Supabase client is being imported
+// The function 'fetchData' fetches all records from the 'Voedingswaarden' table, the 'fetchData' function is called to retrieve the data
+// An error is being shown when something goes wrong with the fetch, the data is being shown in the console when everything goes fine
+
 "use strict";
 
 import { supabase } from "./supabaseClient.js";
