8161c09403167aa027036d24700f2ddc450372ae
CarysaPeeters <carysa.peeters@student.ehb.be>
2025-01-04 15:47:51 +0100

    build: made a total of calories per day and put it into the graph

--- scripts/js/dataVisualisation.js
+++ scripts/js/dataVisualisation.js
@@ -1,24 +1,61 @@
 "use strict";
 
-import {fetchData} from './retrieveData.js'
+import { fetchData } from "./retrieveData.js";
 
-const ctx = document.getElementById('myChart');
+const labels = [];
+const calorieData = [];
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+async function processDataForChart() {
+	const data = await fetchData();
+	if (!data) {
+		console.error("No data found");
+		return;
+	}
+
+	const dailyCalories = {};
+
+	data.forEach(function (row) {
+		const date = new Date(row.datumTijd);
+		const formattedDate = date.toLocaleDateString();
+
+		if (!dailyCalories[formattedDate]) {
+			dailyCalories[formattedDate] = 0;
+		}
+
+		dailyCalories[formattedDate] += row.calorieën;
+	});
+
+	for (const date in dailyCalories) {
+		labels.push(date);
+		calorieData.push(dailyCalories[date]);
+	}
+
+	generateCaloriesChart(labels, calorieData);
+}
+
+function generateCaloriesChart(labels, calorieData) {
+	const ctx = document.getElementById("myChart");
+
+	new Chart(ctx, {
+		type: "bar",
+		data: {
+			labels: labels,
+			datasets: [
+				{
+					label: "Aantal Calorieën per Dag",
+					data: calorieData,
+					borderWidth: 1,
+				},
+			],
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true,
+				},
+			},
+		},
+	});
+}
+
+processDataForChart();
