7feb8be991eb4c02f9f4d6b0f8cb9651d86a8769
CarysaPeeters <carysa.peeters@student.ehb.be>
2025-01-04 16:32:00 +0100

    build: added the 'totalNutritions' and 'generatePieChart' functions in dataVisualisation.js to create a pie chart to display the nutritional values

--- scripts/js/dataVisualisation.js
+++ scripts/js/dataVisualisation.js
@@ -33,6 +33,44 @@ async function caloriesPerDay() {
 	generateCaloriesChart(labels, calorieData);
 }
 
+async function totalNutritions() {
+	const data = await fetchData();
+	if (!data) {
+		console.error("No data found");
+		return;
+	}
+
+	let totaalEiwitten = 0;
+	let totaalKoolhydraten = 0;
+	let totaalVetten = 0;
+	let totaalVezels = 0;
+	let totaalIjzer = 0;
+	let totaalZink = 0;
+	let totaalCalcium = 0;
+	let totaalB12 = 0;
+	let totaalD3 = 0;
+	let totaalOmega3 = 0;
+
+	data.forEach(function (row) {
+		totaalEiwitten += row.proteïne;
+		totaalKoolhydraten += row.koolhydraten;
+		totaalVetten += row.vetten;
+		totaalVezels += row.vezels;
+		totaalIjzer += row.ijzer;
+		totaalZink += row.zink;
+		totaalCalcium += row.calcium;
+		totaalB12 += row.b12;
+		totaalD3 += row.d3;
+		totaalOmega3 += row.omega3;
+	});
+
+	const nutrientLabels = ["Eiwitten (g)", "Koolhydraten (g)", "Vetten (g)", "Vezels (g)", "Ijzer (mg)", "Zink (mg)", "Calcium (mg)", "B12 (µg)", "D3 (µg)", "Omega-3 (g)"];
+
+	const nutrientData = [totaalEiwitten, totaalKoolhydraten, totaalVetten, totaalVezels, totaalIjzer, totaalZink, totaalCalcium, totaalB12, totaalD3, totaalOmega3];
+
+	generatePieChart(nutrientLabels, nutrientData);
+}
+
 function generateCaloriesChart(labels, calorieData) {
 	const ctx = document.getElementById("myChart");
 
@@ -42,7 +80,7 @@ function generateCaloriesChart(labels, calorieData) {
 			labels: labels,
 			datasets: [
 				{
-					label: "Aantal Calorieën per Dag",
+					label: "Aantal calorieën per dag",
 					data: calorieData,
 					borderWidth: 1,
 				},
@@ -58,4 +96,24 @@ function generateCaloriesChart(labels, calorieData) {
 	});
 }
 
+function generatePieChart(labels, data) {
+	const ctx = document.getElementById("myPieChart");
+
+	new Chart(ctx, {
+		type: "pie",
+		data: {
+			labels: labels,
+			datasets: [
+				{
+					label: "Totaal Voedingsstoffen",
+					data: data,
+					borderWidth: 1,
+				},
+			],
+		},
+		options: {},
+	});
+}
+
 caloriesPerDay();
+totalNutritions();
