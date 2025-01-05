e52a53e8008ccdcf5288f26d430f84b4482024f9
CarysaPeeters <carysa.peeters@student.ehb.be>
2025-01-05 17:34:19 +0100

    build: made the colors for each chart uniform

--- scripts/js/dataVisualisationPie.js
+++ scripts/js/dataVisualisationPie.js
@@ -28,7 +28,7 @@ async function totalNutritions() {
 
 	// There is an iteration over each row and the calculations for the total of each nutrient are being made
 	data.forEach(function (row) {
-		totaalEiwitten += row.proteïne;
+		totaalEiwitten += row.eiwitten;
 		totaalKoolhydraten += row.koolhydraten;
 		totaalVetten += row.vetten;
 		totaalVezels += row.vezels;
@@ -59,6 +59,30 @@ function generatePieChart(labels, data) {
 				{
 					label: "Totaal Voedingsstoffen",
 					data: data,
+					backgroundColor: [
+						"rgba(255, 99, 132, 0.2)",
+						"rgba(54, 162, 235, 0.2)",
+						"rgba(255, 206, 86, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+						"rgba(153, 102, 255, 0.2)",
+						"rgba(255, 159, 64, 0.2)",
+						"rgba(201, 203, 207, 0.2)",
+						"rgba(103, 58, 183, 0.2)",
+						"rgba(0, 188, 212, 0.2)",
+						"rgba(0, 150, 136, 0.2)",
+					],
+					borderColor: [
+						"rgba(255, 99, 132, 1)",
+						"rgba(54, 162, 235, 1)",
+						"rgba(255, 206, 86, 1)",
+						"rgba(75, 192, 192, 1)",
+						"rgba(153, 102, 255, 1)",
+						"rgba(255, 159, 64, 1)",
+						"rgba(201, 203, 207, 1)",
+						"rgba(103, 58, 183, 1)",
+						"rgba(0, 188, 212, 1)",
+						"rgba(0, 150, 136, 1)",
+					],
 					borderWidth: 1,
 				},
 			],
